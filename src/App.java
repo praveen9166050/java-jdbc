c65d024d50b264061f76ea06b1ad9b4fcb834ed4
Praveen Kumar <praveen9166050@gmail.com>
2024-11-06 12:10:10 +0530

    Prepared statements in JDBC

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-// import java.sql.PreparedStatement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,14 +15,36 @@ public class App {
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection successful");
 
-            // createAnEmployee(connection);
+            createAnEmployee(connection);
             // deleteAnEmployee(connection);
-            updateAnEmployee(connection);
-            getAllEmployees(connection);
+            // updateAnEmployee(connection);
+            // getAllEmployees(connection);
+            getAnEmployee(connection);
+
 
             connection.close();
         } catch (SQLException e) {
             System.out.println("Connection failed: " + e.getMessage());
+        }
+    }
+
+    public static void getAnEmployee(Connection connection) {
+        try {
+            String query = "SELECT * FROM employees WHERE ID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, 2);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println("==================================================");
+                System.out.println("ID: " + resultSet.getInt("id"));
+                System.out.println("Name: " + resultSet.getString("name"));
+                System.out.println("Role: " + resultSet.getString("role"));
+                System.out.println("Salary: " + resultSet.getDouble("salary"));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
         }
     }
 
@@ -46,25 +68,25 @@ public class App {
 
     public static void createAnEmployee(Connection connection) {
         try {
-            // String insertQuery = "INSERT INTO employees (id, name, role, salary) VALUE (?, ?, ?, ?)";
+            String insertQuery = "INSERT INTO employees (id, name, role, salary) VALUE (?, ?, ?, ?)";
 
-            // PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-            // preparedStatement.setInt(1, 4);
-            // preparedStatement.setString(2, "Rohit");
-            // preparedStatement.setString(3, "Frontend");
-            // preparedStatement.setDouble(4, 70000.0);
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setInt(1, 4);
+            preparedStatement.setString(2, "Rohit");
+            preparedStatement.setString(3, "Frontend");
+            preparedStatement.setDouble(4, 70000);
 
-            // int rowsAffected = preparedStatement.executeUpdate(insertQuery);
+            int rowsAffected = preparedStatement.executeUpdate();
 
-            Statement statement = connection.createStatement();
-            String insertQuery = "INSERT INTO employees (id, name, role, salary) VALUE (4, 'Rohit', 'Frontend', 70000)";
-            int rowsAffected = statement.executeUpdate(insertQuery);
+            // Statement statement = connection.createStatement();
+            // String insertQuery = "INSERT INTO employees (id, name, role, salary) VALUE (4, 'Rohit', 'Frontend', 70000)";
+            // int rowsAffected = statement.executeUpdate(insertQuery);
             if (rowsAffected > 0) {
                 System.out.println("Employee created sucessfully");
             } else {
                 System.err.println("Insertion failed");
             }
-            statement.close();
+            preparedStatement.close();
         } catch (SQLException e) {
             System.out.println("SQL Error: " + e.getMessage());
         }
